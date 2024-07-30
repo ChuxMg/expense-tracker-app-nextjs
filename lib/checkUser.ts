5a175c5b52cb2ccb09c6cf5df302f05e823dd852
ChuxMg <chuxmgbojikwe@gmail.com>
2024-07-30 18:34:13 +0700

    Logged-in user data updated to db

---
+++ lib/checkUser.ts
@@ -0,0 +1,40 @@
+import {db} from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { use } from "react";
+
+export const checkUser = async () => {
+    const user = await currentUser();
+
+    // Check for current logged in clerk user
+
+    if(!user) {
+        return null;
+    }
+
+    // Check if the user is alread in the database
+
+    const loggedInUser = await db.user.findUnique({
+        where: {
+            clerkUserId: user.id
+        }
+    })
+
+    // If user is in database, return user
+
+    if(loggedInUser) {
+        return loggedInUser;
+    }
+
+    // If not in database, create new user
+
+    const newUser = await db.user.create({
+        data: {
+            clerkUserId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0].emailAddress,
+        }
+    });
+
+    return newUser;
+};
