c0c0216bd1a06e57f1cd0ee3c4bae28086333188
ChuxMg <chuxmgbojikwe@gmail.com>
2024-07-30 20:21:18 +0700

    Implement dynamic welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import { Guest } from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
+
   return (
     <main>
-      <h1>Expenses Tracker</h1>
+      <h1>Welcome, {user.firstName}</h1>
     </main>
   );
 };
